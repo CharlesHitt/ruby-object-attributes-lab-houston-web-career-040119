16e03b30910a536c76e2e5391ac16f244bffd58f
Charles Hitt <charles.a.hitt@gmail.com>
2019-04-03 13:14:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,7 @@ class Dog
   
   def initialize
     @name = name
+    @breed = breed
   end
   
   def name
@@ -12,5 +13,12 @@ class Dog
     @name = (new_name)
   end
   
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = (new_breed)
+  end
   
 end
